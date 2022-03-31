5d3f184125485effffaca34c9d4e8a2580587494
jollyfellow7 <simonecompagnone7@gmail.com>
2022-03-31 13:16:51 +0200

    Add project files.

---
+++ HelloCscharp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Benvenuto!");
+Console.Write("Inserisci il tuo mese preferito: ");
+var mese = Console.ReadLine();
+
+Console.WriteLine("Il mese di " + mese + " , piace anche a me!");
+Console.WriteLine("Arrivederci!");
